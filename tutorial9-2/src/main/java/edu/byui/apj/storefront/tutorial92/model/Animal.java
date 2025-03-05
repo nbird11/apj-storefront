e94ed48d44c9eca5ce20997a14a5e9a27e5c231b
Nathan Bird <nathanbirdka@gmail.com>
2025-03-05 15:18:43 -0700

    feat: implement tutorial9-2

---
+++ tutorial9-2/src/main/java/edu/byui/apj/storefront/tutorial92/model/Animal.java
@@ -0,0 +1,12 @@
+package edu.byui.apj.storefront.tutorial92.model;
+
+import lombok.Data;
+
+@Data
+public class Animal {
+    private int id;
+    private String name;
+    private int age;
+    private String diet;
+    private String species;
+}
