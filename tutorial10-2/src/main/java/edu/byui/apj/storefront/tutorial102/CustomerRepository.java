176dd535fcf3efbd098c41277993fefc4ce438ac
Nathan Bird <nathanbirdka@gmail.com>
2025-03-12 16:04:46 -0600

    feat: setup tutorial10-2, complete tutorials

---
+++ tutorial10-2/src/main/java/edu/byui/apj/storefront/tutorial102/CustomerRepository.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial102;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+
+    public Customer findByFirstName(String firstName);
+
+    public List<Customer> findByLastName(String lastName);
+
+}
---
+++ tutorial10-2/src/test/java/edu/byui/apj/storefront/tutorial102/Tutorial102ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
