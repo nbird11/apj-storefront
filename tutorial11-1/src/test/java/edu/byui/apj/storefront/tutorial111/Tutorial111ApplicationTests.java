6a0c0db25502d5ef46124eaa41b67b62eaddd01b
Nathan Bird <nathanbirdka@gmail.com>
2025-03-17 15:01:31 -0600

    feat: complete tutorial11-1

---
+++ tutorial11-1/src/test/java/edu/byui/apj/storefront/tutorial111/Tutorial111ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
