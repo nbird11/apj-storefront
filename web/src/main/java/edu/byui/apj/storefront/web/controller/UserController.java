fa9691f674265bd732b3ab104d227a0d2523a96c
Nathan Bird <nathanbirdka@gmail.com>
2025-03-30 23:19:51 -0600

    fix: replace optional with null check

--- web/src/main/java/edu/byui/apj/storefront/web/controller/UserController.java
+++ web/src/main/java/edu/byui/apj/storefront/web/controller/UserController.java
@@ -7,19 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @GetMapping("/me")
-    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal Optional<UserDetails> maybeUserDetails) {
-        return maybeUserDetails.map(userDetails ->
-            ResponseEntity.ok(userDetails.getUsername())
-        ).orElse(
-            ResponseEntity.noContent().build()
-        );
+    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return userDetails != null
+            ? ResponseEntity.ok(userDetails.getUsername())
+            : ResponseEntity.noContent().build();
     }
 
 }
